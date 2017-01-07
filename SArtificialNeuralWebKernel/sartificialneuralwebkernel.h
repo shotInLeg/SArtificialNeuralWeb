f0775aef4fa45013a14d94774c56226781459c87
shotInleg <shotInLeg@yandex.ru>
2017-01-07 19:10:35 +0300

    add header in kernel

--- SArtificialNeuralWebKernel/sartificialneuralwebkernel.h
+++ SArtificialNeuralWebKernel/sartificialneuralwebkernel.h
@@ -8,13 +8,31 @@
 
 using NeuronMap = QVector< QVector< int > >;
 
+struct NeuronAnswer
+{
+    QString id;
+    QString name;
+    int rate;
+};
+
 class SArtificialNeuralWebKernel
 {
 public:
     class Neuron
     {
     public:
-        Neuron(){}
+        Neuron()
+        {}
+
+        Neuron( const QString& id, const QString& name, const NeuronMap& map )
+        {}
+
+        int overlap( const NeuronMap& map ) const
+        {}
+
+        void rateUp( const NeuronMap& map )
+        {}
+
     private:
         int _rate;
         QString _id;
@@ -22,9 +40,30 @@ public:
         NeuronMap _map;
     };
 
-    SArtificialNeuralWebKernel();
+    SArtificialNeuralWebKernel(){}
+
+    QVector< NeuronAnswer > overlaps( const NeuronMap& map )
+    {
+
+    }
+
+    void rateUpNeuron( const QString& id, const NeuronMap& map )
+    {
+
+    }
+
+    void addNeuron( const QString& name, const NeuronMap& map )
+    {
+
+    }
+
+    static NeuronMap neuronMapFromBitmap( const QBitmap& bitmap )
+    {}
+
 protected:
     QMap<QString, Neuron> _neuralWeb;
 };
+
+using SANWK = SArtificialNeuralWebKernel;
 
 #endif // SARTIFICIALNEURALWEBKERNEL_H
